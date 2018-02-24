eb8d65313c5dd8dd60cc6d78af5b99b5a1212df1
GlebDev <webdev.glib@gmail.com>
2018-02-24 22:50:07 +0200

    second commit

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+
+	[SerializeField] private Animator RobotAnimator;
+	[SerializeField] private ActionState[] sequencing;
+	public int CurAction = -1;
+	private ActionState robotState;
+	public enum ActionState {
+		None,
+		Iddle,
+		Action1,
+		Action2,
+		Emergence,
+		Disappearance
+	}
+
+	private ActionState RobotState{
+		get{
+			return robotState;
+		}
+		set{ 
+			switch (value) {
+			case ActionState.None:
+				RobotAnimator.SetTrigger("Disappearance");
+				break;
+			case ActionState.Iddle:
+				break;
+			case ActionState.Emergence:
+				RobotAnimator.SetTrigger("Emergence");
+				break;
+			case ActionState.Disappearance:
+				RobotAnimator.SetTrigger("Disappearance");
+				break;
+			case ActionState.Action1:
+				RobotAnimator.SetTrigger("Action1");
+				break;
+			case ActionState.Action2:
+				RobotAnimator.SetTrigger("Action2");
+				break;
+			}
+		}
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public void ShowRobot(){
+		ChangeActionState ();
+	}
+
+	public void ChangeActionState(){
+		if (CurAction < sequencing.Length-1) {
+			RobotState = sequencing [++CurAction];
+		} else {
+			CurAction = sequencing.Length-1;
+
+		}
+
+	}
+
+
+		
+}
